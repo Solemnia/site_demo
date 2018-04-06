b2a1e137eb05c270ac10fae880182dc8c6d49610
Solène CLOAREC <sgc.cloarec@gmail.com>
2018-04-06 10:33:19 +0200

    ajout css

--- index.php
+++ index.php
@@ -3,8 +3,14 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
   <title>Site demo</title>
+  <style>
+  h1{
+    position:relative;
+    top: 50%;
+    font-size: 30px;
+    color: aqua;
+  }</style>
 </head>
 <body>
   <h1>Bonjour tout le monde ! C'est vendredi !</h1>
